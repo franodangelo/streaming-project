68f249b7f504bfdfb962a984a5f83290c0f6f43a
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-02 10:47:11 -0300

    app with browser router applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import "swiper/swiper.min.css";
 import "./App.scss";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Router from "./config/Router";
 
 function App() {
-    return <></>;
+    return (
+        <BrowserRouter>
+            <Route render={props => {
+                <>
+                    <Header {...props} />
+                    <Router />
+                    <Footer />
+                </>
+            }} />
+        </BrowserRouter>
+    )
 }
 
 export default App;
