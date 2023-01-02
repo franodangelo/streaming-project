56376ff22952d35c675a36fecb947ceb8d839bc5
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-02 11:50:16 -0300

    rendering components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "swiper/swiper.min.css";
 import "./App.scss";
-import { BrowserRouter, Route } from "react-router-dom";
+
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Router from "./config/Router";
@@ -8,13 +9,9 @@ import Router from "./config/Router";
 function App() {
     return (
         <BrowserRouter>
-            <Route render={props => {
-                <>
-                    <Header {...props} />
-                    <Router />
-                    <Footer />
-                </>
-            }} />
+            <Header />
+            <Router />
+            <Footer />
         </BrowserRouter>
     )
 }
