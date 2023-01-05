00c5b80b7e11e116b36233ea2e26ecd396eac4b9
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-05 10:46:03 -0300

    creating slider

---
+++ src/components/HeroSlide/HeroSlide.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import apiConfig from "../../api/apiConfig";
+import tmdbApi, { category, movieType } from "../../api/tmdbApi";
+
+export default function HeroSlide() {
+
+    const [movieItems, setMovieItems] = useState([]);
+
+    useEffect(() => {
+        async function getMovies() {
+            const params = { page: 1 };
+            try {
+                const response = await tmdbApi.getMoviesList(movieType.popular, { params });
+                setMovieItems(response.results.slice(0, 4));
+                console.log(response);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getMovies();
+    }, [])
+
+    return (
+        <div className="hero-slide">
+            HeroSlide
+        </div>
+    )
+}
