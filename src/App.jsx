52a31b5cc4c3e51760ddb3eb9f7f8ec1b7a4231a
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-05 10:45:36 -0300

    trying routing without router in config folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import "swiper/swiper.min.css";
 import "./App.scss";
 
-import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Router from "./config/Router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Catalog from "./views/Catalog";
+import Home from "./views/Home";
+import Detail from "./views/Detail";
 
 function App() {
     return (
         <BrowserRouter>
             <Header />
-            <Router />
+            <Routes>
+                <Route
+                    path="/:category/search/:keyword"
+                    element={<Catalog />} />
+                <Route
+                    path="/:category/:id"
+                    element={<Detail />} />
+                <Route
+                    path="/:category"
+                    element={<Catalog />} />
+                <Route
+                    path="/"
+                    exact
+                    element={<Home />} />
+            </Routes>
             <Footer />
         </BrowserRouter>
     )
